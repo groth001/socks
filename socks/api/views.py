e74e92dfa652dcfe39deede3a5f1464a34a52cef
groth001 <groth@unomaha.edu>
2020-11-13 19:39:22 -0800

    Add new views (controllers)

--- socks/api/views.py
+++ socks/api/views.py
@@ -7,49 +7,6 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
-class RegionList(APIView):
-    """
-    List all Regions, or create a new Region
-    """
-    def get(self, request, format=None):
-        regions = Region.objects.all()
-        serializer = RegionSerializer(regions, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = RegionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class RegionDetail(APIView):
-    """
-    Retrieve, update or delete a Region.
-    """
-    def get_object(self, pk):
-        try:
-           return Region.objects.get(pk=pk)
-       except Region.DoesNotExist:
-           raise Http404
-
-    def get(self, request, pk, format=None):
-        region = self.get_object(pk)
-        serializer = RegionSerializer(region)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        region = self.get_object(pk)
-        serializer = RegionSerializer(region, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        region = self.get_object(pk)
-        region.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class TeamList(APIView):
     """
@@ -93,4 +50,136 @@ class TeamDetail(APIView):
     def delete(self, request, pk, format=None):
         team = self.get_object(pk)
         team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class RoleList(APIView):
+    """
+    List all Roless, or create a new Role.
+    """
+    def get(self, request, format=None):
+        roles = Role.objects.all()
+        serializer = RoleSerializer(roles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = RoleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class RoleDetail(APIView):
+    """
+    Retrieve, update or delete a Role.
+    """
+    def get_object(self, pk):
+       try:
+           return Role.objects.get(pk=pk)
+       except Role.DoesNotExist:
+           raise Http404
+
+    def get(self, request, pk, format=None):
+        role = self.get_object(pk)
+        serializer = RoleSerializer(role)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        role = self.get_object(pk)
+        serializer = RoleSerializer(role, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        role = self.get_object(pk)
+        role.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class EventList(APIView):
+    """
+    List all Event, or create a new Event.
+    """
+    def get(self, request, format=None):
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EventDetail(APIView):
+    """
+    Retrieve, update or delete a Event.
+    """
+    def get_object(self, pk):
+       try:
+           return Event.objects.get(pk=pk)
+       except Event.DoesNotExist:
+           raise Http404
+
+    def get(self, request, pk, format=None):
+        event = self.get_object(pk)
+        serializer = EventSerializer(team)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        event = self.get_object(pk)
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        event = self.get_object(pk)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class EmployeeList(APIView):
+    """
+    List all Event, or create a new Employee.
+    """
+    def get(self, request, format=None):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EmployeeDetail(APIView):
+    """
+    Retrieve, update or delete a Employee.
+    """
+    def get_object(self, pk):
+       try:
+           return Employee.objects.get(pk=pk)
+       except Employee.DoesNotExist:
+           raise Http404
+
+    def get(self, request, pk, format=None):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(team)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        employee = self.get_object(pk)
+        employee.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
