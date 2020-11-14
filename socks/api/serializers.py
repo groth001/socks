7a9b652f9841f381eaed9faf53dbb6d33558e112
groth001 <groth@unomaha.edu>
2020-11-13 19:06:06 -0800

    Revise serializers

--- socks/api/serializers.py
+++ socks/api/serializers.py
@@ -1,27 +1,22 @@
 from rest_framework import serializers
 from api.models import *
 
-class RegionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Region
-        fields = ['name']
-
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
         fields = ['name']
 
-class EmployeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Region
-        fields = ['name', 'region', 'team']
-
-class ShiftSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Shift
-        fields = ['name']
-
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
-        fields = ['name']
+        fields = ['name', 'isrequired']
+        
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['name', 'starttime', 'endtime']
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ['name', 'email', 'password', 'team', 'rank', 'shift']
