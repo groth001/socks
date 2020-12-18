d77ea55821f1f37f797d0fed70cea3bd6710df71
groth001 <groth@unomaha.edu>
2020-12-18 08:02:07 -0800

    Added client side models

---
+++ socks-gui/app/models/team-detail.js
@@ -0,0 +1,10 @@
+//import Model from '@ember-data/model';
+
+//export default class TeamModel extends Model {
+
+//}
+
+import DS from 'ember-data';
+export default DS.Model.extend({
+  name: DS.attr(),
+});
